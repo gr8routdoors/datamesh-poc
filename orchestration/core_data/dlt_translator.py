900d7cccd5aefd68cbf8ed7db33f62d165f937f0
Devon Berry <dberry@crisistextline.org>
2024-09-05 19:52:03 -0400

    feat: improved naming of DLT created resources

--- orchestration/core_data/dlt_translator.py
+++ orchestration/core_data/dlt_translator.py
@@ -5,17 +5,19 @@ from dagster_embedded_elt.dlt import (
     DagsterDltTranslator,
 )
 
-class PrefixDagsterDltTranslator(DagsterDltTranslator):
+class CanonicalDagsterDltTranslator(DagsterDltTranslator):
     """Allows for setting custom prefixes for DLT assets"""
 
-    def __init__(self, asset_prefix, upstream_prefix):
-        self.asset_prefix = asset_prefix
-        self.upstream_prefix = upstream_prefix    
+    def __init__(self, source_name, destination_name, database_name, schema_name):
+        self.source_name = source_name
+        self.destination_name = destination_name
+        self.database_name = database_name
+        self.schema_name = schema_name    
     
     def get_asset_key(self, resource: DagsterDltResource) -> AssetKey:
         """Overrides asset key to be the dlt resource name."""
-        return AssetKey(f"{self.asset_prefix}__{resource.name}")
+        return AssetKey(f"{self.destination_name}__{self.database_name}__{self.schema_name}__{resource.name}")
 
     def get_deps_asset_keys(self, resource: DagsterDltResource) -> Iterable[AssetKey]:
         """Overrides upstream asset key to be a single source asset."""
-        return [AssetKey(f"{self.upstream_prefix}__{resource.name}")]
+        return [AssetKey(f"{self.source_name}__{self.database_name}__{self.schema_name}__{resource.name}")]
